e89ff60e9d4802668ff0ed1e55782690def98cec
newprog2023 <newprog@internet.ru>
2024-07-07 20:21:38 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Игровой ТИР')
+pygame.display.set_caption('ПРОСТЕНЬКИЙ ТИР - попади в деву из Mortal Kombat')
 icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
-target_image = pygame.image.load('img/MortalKombat100.png')
+target_image = pygame.image.load('img/MortalKombatG100.png')
 target_width = 100
 target_height = 100
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    screen.blit(target_image, (target_x, target_y))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+    pygame.display.update()
 
 pygame.quit()
