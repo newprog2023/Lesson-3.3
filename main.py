0db7b7f86053b06e9465033fe00ef1e45b73f16a
newprog2023 <newprog@internet.ru>
2024-07-07 18:29:09 +0300

    add icon.png

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Игровой ТИР')
-icon = pygame.image.load
+icon = pygame.image.load("img/icon.png»)
+pygame.display.set_icon(icon)
 
 running = True
 while running:
